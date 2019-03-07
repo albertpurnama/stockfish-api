bb2ec22451979d99a847042c0390a7064fbe39e4
Albert Putra Purnama <albertputrapurnama@gmail.com>
2019-03-07 15:36:00 -0800

    initial commit

---
+++ main.py
@@ -0,0 +1,28 @@
+from flask import Flask, request
+from flask_cors import CORS
+import chess
+import chess.engine
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/')
+def hello_world():
+  return 'Hello, World!'
+
+# POST /nextmove
+# chessboard state as payload
+@app.route('/nextmove', methods=['POST'])
+def getNextMove():
+  # init engine
+  engine = chess.engine.SimpleEngine.popen_uci("bin/stockfish")
+  
+  # get the payload
+  req_data = request.data
+  print(req_data.decode('utf-8'))
+
+  board = chess.Board(req_data.decode('utf-8'))
+  result = engine.play(board, chess.engine.Limit(time=0.100))
+  
+  engine.quit()
+  return result.move.uci()
